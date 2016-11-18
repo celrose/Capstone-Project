aeeb043d518260feeb0a550069ee33cbde5c383d
Rocelle <annecadavos@yahoo.com>
2016-11-18 11:15:52 +0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,29 +1,153 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Game {
+class Game implements Runnable{
 	
-	String songTitle;
-	Lyrics l = new Lyrics();
+	 AnimationPanel animation;
+	 JToggleButton start;
+	 Lyrics lyric;
 
-	public Game() {
-		
-		//edit title to be the song chosen
-		songTitle = "Alone Together - FOB";
-		JFrame game = new JFrame(songTitle);
-		//add 3 seconds delay
-		//possibly JTextArea that displays count down 3, 2, 1
-		//thread(1 second)
-		Sound.SONG.play();
+	 @Override
+    public void run() {
 
-		game.setSize(600, 900);
-		game.setResizable(false);
-		game.setLayout(new GridLayout());
-		game.setLocation(600, 30);
+    	JFrame frame = new JFrame("AKKYKSTN");
+        JPanel content = new JPanel(new BorderLayout());
 
-		//game loop
+        start = new JToggleButton("START / PAUSE");
+        content.add(start, BorderLayout.NORTH);
+
+        animation = new AnimationPanel(600, 900);
+        content.add(animation, BorderLayout.CENTER);
+
+		if(false) {
+            start.addActionListener(new ThreadAnimator());
+        } else {
+            start.addActionListener(new TimerAnimator());
+        }
 
 
-		game.setVisible(true);
-	}
+    	frame.setContentPane(content);
+    	frame.setBackground(Color.BLACK);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
+    }
+
+    abstract class Animator implements ActionListener {
+    	final int period = ( 1000 / 60 );
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            if(start.isSelected()) {
+            	Sound.SONG.play();
+                start();
+            } else {
+            	//need to resume
+            	//maybe make a customized stop() in Sound class?
+            	Sound.SONG.stop();
+                stop();
+            }
+        }
+
+        abstract void start();
+        abstract void stop();
+
+        void animate() {
+        	int workingPos = animation.startPosition;
+
+            if(workingPos < animation.getHeight()) {
+                workingPos ++;
+            }
+
+            animation.startPosition = workingPos;
+
+            animation.repaint();
+        }
+       
+   }
+   class ThreadAnimator extends Animator {
+        volatile boolean isRunning;
+
+        Runnable loop = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while(isRunning) {
+                        animate();
+                        Thread.sleep(period);
+                    }
+                } catch(InterruptedException e) {
+                    throw new AssertionError(e);
+                }
+            }
+        };
+
+        @Override
+        void start() {
+            isRunning = true;
+
+            new Thread(loop).start();
+        }
+
+        @Override
+        void stop() {
+            isRunning = false;
+        }
+    }
+
+    class TimerAnimator extends Animator {
+        Timer timer = new Timer(period, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                animate();
+            }
+        });
+
+        @Override
+        void start() {
+            timer.start();
+        }
+
+        @Override
+        void stop() {
+            timer.stop();
+        }
+    }
+
+	static class AnimationPanel extends JPanel {
+
+		volatile int startPosition;
+
+
+    	AnimationPanel(int width, int height) {
+    		setPreferredSize(new Dimension(width, height));
+        	setBackground(Color.BLACK);
+
+        	startPosition = getHeight();
+    	}
+
+    	@Override
+        protected void paintComponent(Graphics g) {
+        	super.paintComponent(g);
+        	String lyric = new String("soul");
+        	int width = getWidth();
+            int height = getHeight();
+
+			g.setColor(Color.WHITE);
+
+			//find a way to customize the string so dako2 cya
+			int currentPosition = startPosition;
+			if(currentPosition < height) {
+				g.drawString(lyric, 30, startPosition);
+				//g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+			}
+
+			
+		}
+    }
+
 }
